46875f70fee883a325d1f850fedf183920c9c5c0
R4my <44817847+R4my@users.noreply.github.com>
2018-11-06 23:51:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ client.on('ready', () => {
 
 
 client.on('message', message => {
-    if (message.content === "$help") {
+    if (message.content === "Rhelp") {
     let embed = new Discord.RichEmbed()
   .setAuthor(message.author.username)
   .setFooter(`© SHYBOY_05 ™.`, 'https://images-ext-2.discordapp.net/external/X9SanEG0s7Dtv3krTgy-kod_fj6JRVJ2AG4JryCiiz0/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/375761288518828042/fffa31c797e88cf059dd6db424ff456a.png?width=80&height=80')
