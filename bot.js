42aab38747d45074f18f895cf43877a83e7bb843
R4my <44817847+R4my@users.noreply.github.com>
2018-11-06 23:10:45 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const moment = require("moment");
+const fs = require("fs");
+const prefix = "R";
+client.on('ready', () => {
+    client.user.setGame(`!~[ Commands | $help ]~!`,"http://twitch.tv/y04zgamer")
+    client.user.setStatus("dnd")
+ });
+
+
+
+
+
+client.login(process.env.BOT_TOKEN);
