82b9dd005eb8cc7fc1b57bb5fb7aa078a816bb49
R4my <44817847+R4my@users.noreply.github.com>
2018-11-07 03:01:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -327,33 +327,7 @@ client.on('message', message => {
 
 
 
-client.on('message', message => {
-    var prefix = "R";
-    
-        if (message.author.id === client.user.id) return;
-        if (message.guild) {
-       let embed = new Discord.RichEmbed()
-        let args = message.content.split(' ').slice(1).join(' ');
-    if(message.content.split(' ')[0] == prefix + 'bc') {
-        if (!args[1]) {
-    message.channel.send("**Rbc <message>**");
-    return;
-    }
-            message.guild.members.forEach(m => {
-       if(!message.member.hasPermission('ADMINISTRATOR')) return;
-                var bc = new Discord.RichEmbed()
-                .addField('» السيرفر :', `${message.guild.name}`)
-                .addField('» المرسل : ', `${message.author.username}#${message.author.discriminator}`)
-                .addField(' » الرسالة : ', args)
-                .setColor('#ff0000')
-                // m.send(`[${m}]`);
-                m.send(`${m}`,{embed: bc});
-            });
-        }
-        } else {
-            return;
-        }
-    });
+
 
 
 
@@ -540,5 +514,61 @@ message.channel.sendEmbed(embed);
    }
 });
 
+client.on('message', message => {
+if(message.author.bot) return;
+              if(!message.channel.guild) return;
+    var prefix = "R";
+    if(message.content.startsWith(prefix + 'bc')) {
+    if(!message.channel.guild) return message.channel.send('**الأمر بالسيرفرات بس**').then(m => m.delete(5000));
+  if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**ماعندك الصلاحية المطلوبة**' );
+    let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+    let copy = "";
+    let request = `تم ارسال من  ${message.author.username}`;
+    if (!args) return message.reply('**ترسل برودكاست فاضي؟**');message.channel.send(`**متأكد؟** \` ${args}\``).then(msg => {
+    msg.react('✅')
+    .then(() => msg.react('❌'))
+    .then(() =>msg.react('✅'))
+
+    let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+    let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
+
+    let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
+    let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
+    reaction1.on("collect", r => {
+    message.channel.send(`**☑ | تم ... ارسال البرودكاست  ${message.guild.members.size} عضو**`).then(m => m.delete(5000));
+    message.guild.members.forEach(m => {
+    var bc = new
+       Discord.RichEmbed()
+       .setColor('#00ff47')
+       .setTitle('**:incoming_envelope:رسالة:incoming_envelope:')
+       .addField('**:diamond_shape_with_a_dot_inside:السيرفر:diamond_shape_with_a_dot_inside:**', message.guild.name)
+       .addField('**:black_joker:المرسل:black_joker:**', message.author.username)
+       .addField(':page_facing_up:الرسالة:page_facing_up:', args)
+       .setThumbnail(message.author.avatarURL)
+       .setFooter(copy, client.user.avatarURL);
+    m.send({ embed: bc })
+    msg.delete();
+    })
+    })
+    reaction2.on("collect", r => {
+    message.channel.send(`**كنسل يا مدير**`).then(m => m.delete(5000));
+    msg.delete();
+    })
+    })
+    }
+let cooldown = new Set();
+let cdseconds = 5;
+if(!message.content.startsWith(prefix)) return;
+  if(cooldown.has(message.author.id)){
+    message.delete();
+  return  message.reply("ان الله مع الصابرين")
+  }
+  if(!message.member.hasPermission("ADMINISTRATOR")){
+    cooldown.add(message.author.id);
+  }
+setTimeout(() => {
+  cooldown.delete(message.author.id)
+}, cdseconds * 1000)
+    });
 
 client.login(process.env.BOT_TOKEN);
